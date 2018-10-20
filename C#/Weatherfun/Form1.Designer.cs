7472f86cafb314f091797a4009b83055386d50fe
unknown <aron_heesakkers@hotmail.com>
2018-10-20 20:20:51 +0200

    CSharp code added

---
+++ C#/Weatherfun/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Weatherfun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cityTextbox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.getReqButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityLabel.Location = new System.Drawing.Point(94, 76);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(128, 48);
+            this.cityLabel.TabIndex = 0;
+            this.cityLabel.Text = "TEST";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            // 
+            // cityTextbox
+            // 
+            this.cityTextbox.Location = new System.Drawing.Point(102, 150);
+            this.cityTextbox.Name = "cityTextbox";
+            this.cityTextbox.Size = new System.Drawing.Size(100, 29);
+            this.cityTextbox.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(320, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // getReqButton
+            // 
+            this.getReqButton.Location = new System.Drawing.Point(102, 213);
+            this.getReqButton.Name = "getReqButton";
+            this.getReqButton.Size = new System.Drawing.Size(120, 32);
+            this.getReqButton.TabIndex = 4;
+            this.getReqButton.Text = "Request";
+            this.getReqButton.UseVisualStyleBackColor = true;
+            this.getReqButton.Click += new System.EventHandler(this.getReqButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1603, 937);
+            this.Controls.Add(this.getReqButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cityTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cityLabel);
+            this.Name = "Form1";
+            this.Text = "Stad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cityTextbox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button getReqButton;
+    }
+}
+
---
+++ C#/Weatherfun/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Weatherfun
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string city;
+        private void GetCity()
+        {
+            string city = cityTextbox.Text;
+        }
+        
+
+        private void GetJason(){
+            string APIkey = "4835cc6a727ba8d950f6866e7a7d287d";
+            string APIreq = $"api.openweathermap.org/data/2.5/weather?q={city}&APPID={APIkey}";            string test = "www.google.com";
+             
+
+            var responseString =  client.GetStringAsync(APIreq);
+        }
+
+        private void getReqButton_Click(object sender, EventArgs e)
+        {
+            GetJason();
+        }
+    }
+
+
+
+    public class DataObject
+    {
+        public string Name { get; set; }
+    }
+
+    public class Class1
+    {
+        private const string URL = "https://sub.domain.com/objects.json";
+        private string urlParameters = "?api_key=123";
+
+        private string APIkey = "4835cc6a727ba8d950f6866e7a7d287d";
+        string APIreq = $"api.openweathermap.org/data/2.5/weather?q=London&APPID={APIkey}";
+
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(APIkey).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            //Make any other calls using HttpClient here.
+
+            //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
